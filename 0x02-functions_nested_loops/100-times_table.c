5f7e77cf670386f1127e1d787e931fc0c6cc5d8a
L4tif4h <latifaoulmourif04@gmail.com>
2023-08-15 18:42:02 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				}
 				else if (k < 10 && j != 0)
-				}
+				{
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
@@ -45,6 +45,6 @@ void print_times_table(int n)
 		}
 	}
 	_putchar('\n');
-}
-}
+		}
+	}
 }
