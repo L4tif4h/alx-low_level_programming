d13ace652184dc30872db37d2c0e09b966d9b8a2
L4tif4h <latifaoulmourif04@gmail.com>
2023-08-29 15:05:50 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,19 @@
 #include "main.h"
 /**
-* _strstr - Locates a substring.
-* @haystack: Pointer to the string in which to search for the substring.
-* @needle: Pointer to the substring to locate.
-*
-* Return: Pointer to the beginning of the located substring in @haystack,
-*         or NULL if the substring is not found.
+* print_chessboard - Entry point
+* @a: array
+* Return: Always 0 (Success)
 */
 void print_chessboard(char (*a)[8])
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; i < 8; i++)
-        {
-                for (j = 0; j < 8; j++)
-                {
-                        _putchar(a[i][j]);
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
