7ffe6cfe61766f48e8a88d5a595fb7cad9dd52ee
L4tif4h <latifaoulmourif04@gmail.com>
2023-08-30 12:30:32 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
-
 /**
- * _print_rev_recursion - Prints a string in reverse recursively.
+ * _puts_recursion - Prints a string followed by a new line recursively.
  * @s: The input string.
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s)
 	{
 		_putchar(*s);
-		_puts_rev_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
+
+	else
+		_putchar('\n');
 }
